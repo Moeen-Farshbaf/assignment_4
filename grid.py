e070ca6929ff4771a91ebdbdd57ae8febc5c1255
Moeen-Farshbaf <88230820+Moeen-Farshbaf@users.noreply.github.com>
2021-08-11 01:38:35 +0430

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,12 +1,23 @@
 n = int(input('enter width'))
 m = int(input('enter lenght'))
-def width():
+def widthh():
     for i in range (0,n):
-        if i%2>0:
+        if i%2==0:
             print("*", end='')
         else:
             print ('#',end='')
+def width():
+    for i in range (0,n):
+        if i%2==0:
+            print("#", end='')
+        else:
+            print ('*',end='')
         
 for i in range (0,m):
-    width()
-    print('')
+    if i%2==0:
+        widthh()
+        print("")
+    else:
+        width()
+        print('')
+   
