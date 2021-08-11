e4d367fe22c8839c05897037702dacf6c2101524
Moeen-Farshbaf <88230820+Moeen-Farshbaf@users.noreply.github.com>
2021-08-11 12:15:41 +0430

    Add files via upload

---
+++ wordcounter.py
@@ -0,0 +1,16 @@
+active = True
+while active:
+    sen = input("Enter a sentence and I'll count the words.")
+
+    def word_counter():
+        count = 0
+        for ' ' in sen:
+            count = count + 1
+    will = 'are you ready?(yes/no)'
+    if will == 'yes':
+        word_counter()
+        
+    else:
+        pass
+print (count)
+    
